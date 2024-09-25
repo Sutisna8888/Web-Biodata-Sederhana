482e4e59cd9632af721924cce6d230ab8196fa03
Sutisna8888 <161696845+Sutisna8888@users.noreply.github.com>
2024-09-25 16:45:18 +0700

    Add files via upload

---
+++ Biodata.js
@@ -0,0 +1,14 @@
+const nama = document.querySelector("#nama");
+nama.textContent = "Sutisna";
+
+const ttl = document.querySelector("#ttl");
+ttl.textContent = "Cianjur 5 Mei 2004";
+
+const jk = document.querySelector("#jk");
+jk.textContent = "Laki-laki";
+
+const agama = document.querySelector("#agama");
+agama.textContent = "Islam";
+
+const alamat = document.querySelector("#alamat");
+alamat.textContent = "Cibolang Girang";
